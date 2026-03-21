0fe0d780f065754be84851818163d8cb8327dd34
danyventura5681-boop <danyventura5681@gmail.com>
2026-03-21 10:02:58 -0400

    Actualizar keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from threading import Thread
+import os
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "LinkForge is alive 🚀"
+
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
