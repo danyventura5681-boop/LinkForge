cb7c60b9a1b1ab96d82acc8f767bc0ca804f5f4b
danyventura5681-boop <danyventura5681@gmail.com>
2026-03-22 13:29:19 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 from keep_alive import keep_alive
 
+# importar handler
+from handlers.start import start
+
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-
-async def start(update, context):
-    await update.message.reply_text(
-        "🚀 LinkForge está activo."
-    )
 
 
 def main():
     if not BOT_TOKEN:
         raise ValueError("BOT_TOKEN no encontrado")
 
+    # Crear aplicación
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
+    # Registrar handlers
     app.add_handler(CommandHandler("start", start))
 
-    print("✅ Bot iniciado...")
+    print("✅ Bot iniciado correctamente...")
+
+    # Iniciar bot
     app.run_polling()
 
 
