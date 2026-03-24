7d1f94b57fe10b4008ed52f0af3b30f0348787ba
danyventura5681-boop <danyventura5681@gmail.com>
2026-03-24 15:33:43 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
+from keep_alive import keep_alive
 from handlers.start import start
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 def main():
-
     if not BOT_TOKEN:
         raise ValueError("BOT_TOKEN no encontrado")
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
+    # handlers
     app.add_handler(CommandHandler("start", start))
 
-    print("✅ Bot iniciado correctamente")
+    print("✅ Bot iniciado correctamente...")
 
+    # ✅ ÚNICO polling correcto
     app.run_polling()
 
 
 if __name__ == "__main__":
+    keep_alive()
     main()
