7f66195c9a7306cdfcf30d0d232d87389f45bda1
danyventura5681-boop <danyventura5681@gmail.com>
2026-04-04 17:22:58 -0400

    Actualizar start.py

--- handlers/start.py
+++ handlers/start.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
-from database.database import get_user, create_user, get_user_rank, get_user_links
+from database import get_user, create_user, get_user_rank, get_user_links
 
 logger = logging.getLogger(__name__)
 
